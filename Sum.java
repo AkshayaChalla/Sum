8ea35d7d6712e48f18c53a7d77ebd0d44b026360
AkshayaChalla <akshaya.challa215@gmail.com>
2025-01-29 23:00:27 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum{
+    public static void main(String [] args){
+        int a = 3;
+        int b = 4;
+        //Sum = a +b;
+        
+        System.out.println("sum of two is: " + (a + b));
+        System.out.println("SUbtraction of two is:" + (a - b));
+        System.out.println("MUltiplication of two is:" + (a * b));
+        System.out.println("Division of two are:" + (a / b));
+        System.out.println("Modulus of twso are:" + (a % b));
+
+    }
+}
